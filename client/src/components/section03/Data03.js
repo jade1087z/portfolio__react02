eb202d6aa667346a64ef5762e34655d1893623cc
imjonghan <danziro57@gmail.com>
2024-01-15 12:46:50 +0900

    123

--- client/src/components/section03/Data03.js
+++ client/src/components/section03/Data03.js
@@ -1,29 +1,35 @@
+import pro01 from "../../assets/img/proj01.png";
+import pro02 from "../../assets/img/proj02.png";
+import pro03 from "../../assets/img/proj03.png";
+
 const Data03 = [
     {
-        subTitle: '게시판 사이트 제작',
-        h4: 'Crafted a PHP based community platform.',
-        descLink: 'Community Site Link',
-        mainP: 'DB와 SQL 기본 지식을 토대로 서버와 클라이언트 통신을 구현했습니다. RESTFULL API와 데이터베이스를 직접 설계를 해보며 웹사이트 개발의 전체적인 흐름을 익히는데 많은 도움이 되었습니다.',
-        siteLink: '#',
-        gitLink: 'https://github.com/jade1087z/php__CJJD.git',
-    }, 
+        subTitle: "게시판 사이트 제작",
+        img: { pro01 },
+        h4: "Crafted a PHP based community platform.",
+        descLink: "Community Site Link",
+        mainP: "DB와 SQL 기본 지식을 토대로 서버와 클라이언트 통신을 구현했습니다. RESTFULL API와 데이터베이스를 직접 설계를 해보며 웹사이트 개발의 전체적인 흐름을 익히는데 많은 도움이 되었습니다.",
+        siteLink: "#",
+        gitLink: "https://github.com/jade1087z/php__CJJD.git",
+    },
     {
-        subTitle: '유튜브 사이트 제작',
-        h4: 'Built a React YouTube Clone on Digital Stage.',
-        descLink: 'YouTube Clone Link',
-        mainP: '유튜브 API를 불러와 클론 사이트를 제작했습니다. React 그리고, API에 대해 경험 할 수 있는 기회였습니다. React Hook, Redux, Props의 이해를 위한 첫 발걸음이 되었습니다.',
-        siteLink: 'https://fessionyoutube.netlify.app/ ',
-        gitLink: 'https://github.com/jade1087z/reactyoutube.git'
-    }, 
+        subTitle: "유튜브 사이트 제작",
+        img: { pro02 },
+        h4: "Built a React YouTube Clone on Digital Stage.",
+        descLink: "YouTube Clone Link",
+        mainP: "유튜브 API를 불러와 클론 사이트를 제작했습니다. React 그리고, API에 대해 경험 할 수 있는 기회였습니다. React Hook, Redux, Props의 이해를 위한 첫 발걸음이 되었습니다.",
+        siteLink: "https://fessionyoutube.netlify.app/ ",
+        gitLink: "https://github.com/jade1087z/reactyoutube.git",
+    },
     {
-        subTitle: '다이어리 사이트 제작',
-        h4: 'Engineered a diary site with React.js and Node.js.',
-        descLink: 'Diary Site Link',
-        mainP: '비동기 프로그래밍의 문제점이 Virtual DOM에 영향을 미치는 점을 파악하고, 이에 대응하기 위해 FLUX 패턴의 중요성과 Redux, React Hook과 같은 도구들의 역할을 이해했습니다.',
-        siteLink: '#',
-        gitLink: 'https://github.com/jade1087z/KiCanDiary.git',
+        subTitle: "다이어리 사이트 제작",
+        img: { pro03 },
+        h4: "Engineered a diary site with React.js and Node.js.",
+        descLink: "Diary Site Link",
+        mainP: "비동기 프로그래밍의 문제점이 Virtual DOM에 영향을 미치는 점을 파악하고, 이에 대응하기 위해 FLUX 패턴의 중요성과 Redux, React Hook과 같은 도구들의 역할을 이해했습니다.",
+        siteLink: "#",
+        gitLink: "https://github.com/jade1087z/KiCanDiary.git",
+    },
+];
 
-    }, 
-]
-   
 export default Data03;
--- client/src/components/section03/Section03.jsx
+++ client/src/components/section03/Section03.jsx
@@ -26,81 +26,32 @@ const Section03 = () => {
 
             <div className="site__wrap">
                 {Data03.map((data, key) => (
-                    <div className={`site s`+(key+1)}  key={key}>
-                    <span className="num">{key+1}</span>
-                    <div className="subTitle">({data.subTitle})</div>
-                    <div className="tit">
-                        <div className="h4">
-                            {data.h4}
-                        </div>
-                        <div className="img-wrap">
-                            <div className="img"></div>
-                            <div className="desc">
-                                <h4><Link to={data.siteLink}>{data.descLink}</Link></h4>
-                                <p className="">
-                                  {data.mainP}
-                                </p>
-                                <div className="link">
-                                    <Link to={data.gitLink}>View on Git</Link>
+                    <div className={`site s` + (key + 1)} key={key}>
+                        <span className="num">{key + 1}</span>
+                        <div className="subTitle">({data.subTitle})</div>
+                        <div className="tit">
+                            <div className="h4">{data.h4}</div>
+                            <div className="img-wrap">
+                                <div className="img">
+                                    <img src={data.img} alt="" />
+                                </div>
+                                <div className="desc">
+                                    <h4>
+                                        <Link to={data.siteLink}>
+                                            {data.descLink}
+                                        </Link>
+                                    </h4>
+                                    <p className="">{data.mainP}</p>
+                                    <div className="link">
+                                        <Link to={data.gitLink}>
+                                            View on Git
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </div>  
-
                 ))}
-
-                <div className="site s2">
-                    <span className="num">2.</span>
-                    <div className="subTitle">(유튜브 사이트 제작)</div>
-                    <div className="tit">
-                        <div className="h4">
-                            Built a React YouTube Clone on Digital Stage.
-                        </div>
-                        <div className="img-wrap">
-                            <div className="img"></div>
-                            <div className="desc">
-                                <h4><Link to={'#'}>YouTube Clone Link</Link></h4>
-                                <p>
-                                    유튜브 API를 불러와 클론 사이트를
-                                    제작했습니다. React 그리고, API에
-                                    대해 경험 할 수 있는 기회였습니다.
-                                    React Hook, Redux, Props의 이해를 위한 첫
-                                    발걸음이 되었습니다.
-                                </p>
-                                <div className="link">
-                                    <Link to={'#'}>View on Git</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="site s3">
-                    <span className="num">3.</span>
-                    <div className="subTitle">(다이어리 사이트 제작)</div>
-                    <div className="tit">
-                        <div className="h4">
-                            Engineered a diary site with React.js and Node.js.
-                        </div>
-                        <div className="img-wrap">
-                            <div className="img"></div>
-                            <div className="desc">
-                                <h4><Link to={'#'}>Diary Site Link</Link></h4>
-                                <p>
-                                    비동기 프로그래밍의 문제점이 Virtual DOM에{" "}
-                                    영향을 미치는 점을 파악하고, 이에 대응하기{" "}
-                                    위해 FLUX 패턴의 중요성과 Redux, React
-                                    Hook과 같은 도구들의 역할을 이해했습니다.{" "}
-                                    
-                                </p>
-                                <div className="link">
-                                    <Link to={'#'}>View on Git</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
