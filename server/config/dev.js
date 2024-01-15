81c07a547ce6bbb5c242242fd9ed896f84e8b0f8
imjonghan <danziro57@gmail.com>
2024-01-15 12:58:22 +0900

    z

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI:
-        "mongodb+srv://danziro57:zz3125@cluster0.vyemutk.mongodb.net/?retryWrites=true&w=majority",
-};
--- server/config/production.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    // 네이버 클라우드
-    access_key: process.env.S3_KEY,
-    secret_key: process.env.S3_SECRET
-}
